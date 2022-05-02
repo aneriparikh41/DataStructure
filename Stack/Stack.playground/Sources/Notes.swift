d8e2331e4f4a31de66b027e5961de9635d861070
Aneri Parikh <aneri.parikh@walmartlabs.com>
2022-05-02 10:12:59 +0530

    no message

--- Stack/Stack.playground/Sources/Notes.swift
+++ Stack/Stack.playground/Sources/Notes.swift
@@ -10,4 +10,9 @@ import Foundation
 
  3. Reverse in loop
  for i in arr.reversed()
+
+ 4. Dict index access
+ accessing the key of the dict [String: [Any]] at a particular index:
+ Array(dict)[indexPath.section].key
+ Array(dict)[indexPath.section].value
  */
